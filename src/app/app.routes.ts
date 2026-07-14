b8ace7d2c7f70ac66afe255c45c8c462b43e8664
Haidy Hosam <haidyhosam93@gmail.com>
2026-07-14 11:49:25 +0300

    initial FMS UI

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,46 @@
 import { Routes } from '@angular/router';
+import { Login } from './pages/login/login';
+import { Dashboard } from './pages/dashboard/dashboard';
+import { Files } from './pages/files/files';
+import { FileDetails } from './pages/file-details/file-details';
+import { Departments } from './pages/departments/departments';
+import { Users } from './pages/users/users';
+import { Roles } from './pages/roles/roles';
+import { MainLayout } from './layout/main-layout/main-layout';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    component: Login,
+  },
+  {
+    path: '',
+    component: MainLayout,
+    children: [
+      {
+        path: 'dashboard',
+        component: Dashboard,
+      },
+      {
+        path: 'files',
+        component: Files,
+      },
+      {
+        path: 'files/:id',
+        component: FileDetails,
+      },
+      {
+        path: 'departments',
+        component: Departments,
+      },
+      {
+        path: 'users',
+        component: Users,
+      },
+      {
+        path: 'roles',
+        component: Roles,
+      },
+    ],
+  },
+];
---
+++ src/app/pages/file-details/file-details.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-file-details',
+  imports: [],
+  templateUrl: './file-details.html',
+  styleUrl: './file-details.css',
+})
+export class FileDetails {
+
+}
