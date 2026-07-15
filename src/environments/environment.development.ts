efa87e43c4516cb69df352d15baac910599fcb46
Haidy Hosam <haidyhosam93@gmail.com>
2026-07-15 14:18:19 +0300

    try to intigirate Front and Back

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,11 @@
-export const environment = {};
+// environment.development.ts
+export const environment_Back = {
+  production: false,
+  apiUrl: 'http://localhost:8080/api'
+};
+
+// environment.ts
+export const environment = {
+  production: true,
+  apiUrl: 'https://your-production-url/api'
+};
